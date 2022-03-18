bf87a14abd6259fb4f8a0ca9d94ce0a8ca5bb49d
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2022-03-18 19:25:59 +0330

    fixed

--- src/compunets/inputPrice.jsx
+++ src/compunets/inputPrice.jsx
@@ -1,25 +1,25 @@
+import { useState } from "react";
 import "./inputPrice.css"
 let val = 0;
 const InputPrice = (props) => {
-
- function newPrice(e) {
+  
+    function newPrice(e) {
      val = e.target.value;
-     console.log(val);
     };
     
     function subhandel(e) {
      e.preventDefault();
     };
     
- const changePrice = () => {
-   val != 0 ? props.changePrice(val) : alert("no inter a value");
-   val = 0;
+    const changePrice = () => {
+        val != 0 ? props.changePrice(val) : alert("no inter a value");
+        val = 0;
     };
 
    return (
      <>
        <form onSubmit={subhandel} className="input">
-         <input onChange={newPrice} placeholder="newPrice" />
+         <input  onChange={newPrice} placeholder="newPrice" />
          <button onClick={changePrice}>SAVE</button>
        </form>
      </>
